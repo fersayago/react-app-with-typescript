e909a2b7229435755ae6b39f315c483401766533
fersayago <sayago.fernando@gmail.com>
2022-04-23 21:08:15 -0300

    add: map subs de una Api

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,12 @@ import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 import List from './components/List';
-import {Sub} from './components/types';
+import {Sub, SubsResponseFromApi} from './components/types';
 
 interface AppState {
   subs: Array<Sub>
   newSubsNumber: number
 }
-
-const INITIAL_STATE = [
-  {
-    nick: 'suscriptor',
-    subMonths: 3,
-    avatar: 'https://i.pravatar.cc/150?u=sub',
-    description: "Hace de moderador a veces",
-  },
-  {
-    nick: 'suscriptor2',
-    subMonths: 6,
-    avatar: 'https://i.pravatar.cc/150?u=sub2',
-  }
-]
 
 function App() {
   // especificamos entre brackets que tipo de dato va a tener el useState
@@ -34,19 +20,47 @@ function App() {
   // indicamos en el useRef que vamos a usar un elemento div y lo comenzamos nulo
   const divRef = useRef<HTMLDivElement>(null)
 
-
+  // el fetch recibe  un objeto que no es un array de Subs
+  // hay que corregir el fetch ya que typescript no funciona en runtime sino que
+  // valida en estatico o buildtime por lo que se debe
   useEffect(() => {
-    setSubs(INITIAL_STATE);
+    const fetchSubs = (): Promise<SubsResponseFromApi> => {
+      return fetch('http://localhosto:3001/subs').then(res => res.json())
+    }
+
+    const mapFromApiToSubs = (apiResponse: SubsResponseFromApi): Array<Sub> => {
+      return apiResponse.map(subFromApi => {
+        const {
+          nick,
+          months: subMonths,
+          profileUrl: avatar,
+          description
+        } = subFromApi
+
+        return {
+          nick,
+          description,
+          avatar,
+          subMonths
+        }
+      })
+    }
+
+    fetchSubs()
+      .then(mapFromApiToSubs)
+      .then(setSubs)
   }, [])
 
   const handleNewSub = (newSub: Sub): void =>{
     setSubs(subs => [...subs, newSub])
+    setNewSubsNumber(n => n + 1)
   }
 
   return (
     <div className="App" ref={divRef}>
       <h1>Fer Subs</h1>
       <List subs={subs}/>
+      New subs: {newSubsNumber}
       <Form onNewSub={handleNewSub}/>
     </div>
   );
